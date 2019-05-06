73326cc082b53e55059f3d69a0acbfda43f87dc4
sankarshan <sankarshan7@gmail.com>
2019-05-06 20:01:24 +0530

    Update file to get filename

--- fetch_git_info.py
+++ fetch_git_info.py
@@ -1,4 +1,6 @@
 import subprocess
+import sys
+import os 
 
 def get_git_revision_hash():
     return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode("utf8").strip()
@@ -9,7 +11,10 @@ def get_git_revision_short_hash():
 def get_git_branch_name():
     return subprocess.check_output(["git","symbolic-ref", "--short", "HEAD"]).decode("utf8").strip() #[0:-1]
 
+def get_running_filename():
+    return os.path.basename(sys.argv[0])
+
 print("get git revision hash: {}".format(get_git_revision_hash()))
 print("get git revision hash short: {}".format(get_git_revision_short_hash()))
 print("get git branch name: {}".format(get_git_branch_name()))
-
+print("get file name: {}".format(get_running_filename()))
